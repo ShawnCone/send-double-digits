50905c792ac2fad948a59d0c52f7c8b3e2611de2
ShawnCone <deandre.dave@gmail.com>
2023-11-10 12:13:50 -0800

    set up leptop

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,5 @@
-use actix_web::{ get, App, HttpResponse, HttpServer, Responder };
+use leptos::*;
 
-#[get("/ping")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("pong")
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let host = "127.0.0.1";
-    let port = 8080;
-
-    println!("Starting server at {}:{}", host, port);
-
-    HttpServer::new(|| App::new().service(hello))
-        .bind((host, port))?
-        .run().await
+fn main() {
+    mount_to_body(|| view! { <p>"Hello, world!"</p> })
 }
