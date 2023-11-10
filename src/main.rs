7ca6d125439b2ebc2dd3c36aba8624fe9a03a91c
ShawnCone <deandre.dave@gmail.com>
2023-11-10 12:40:19 -0800

    leptos build modules

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+mod components {
+    pub mod app;
+}
+
 use leptos::*;
+use send_double_digits::components::app::App;
 
 fn main() {
-    mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    mount_to_body(App)
 }
